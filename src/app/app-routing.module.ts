bf07a25576b73d671600ca91c3acb517040490e1
Jackson Novaes <jackson.novaes@live.com>
2022-04-25 05:20:55 -0300

    initial commit add styles de footer e nav header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProdutosComponent } from './views/produtos/produtos.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path:"produtos", component: ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
